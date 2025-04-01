46e6c2dd93decada6b03af2615a04d8434b8602c
Tomi\Tomislav <rusevtomy@gmail.com>
2025-04-01 13:56:57 +0300

    Rector fixed

--- tests/Unit/CoinFlipperTest.php
+++ tests/Unit/CoinFlipperTest.php
@@ -2,12 +2,12 @@
 
 use App\CoinFlipper;
 
-test('returns edge when second is divisible by 7', function () {
+test('returns edge when second is divisible by 7', function (): void {
     Carbon\Carbon::setTestNow(Carbon\Carbon::now()->second(14));
     expect(CoinFlipper::flip())->toBe('the coin landed on its edge');
 });
 
-test('returns heads or tails when second is not divisible by 7', function () {
+test('returns heads or tails when second is not divisible by 7', function (): void {
     Carbon\Carbon::setTestNow(Carbon\Carbon::now()->second(13));
     $result = CoinFlipper::flip();
     expect(['heads', 'tails'])->toContain($result);
